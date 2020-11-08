229795409414a62416c817d342b2045ffb99191f
aku134 <60403682+aku134@users.noreply.github.com>
2020-11-08 23:03:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express=require('express');
-const { endianness } = require('os');
-const { mainModule } = require('process');
-const { runInContext } = require('vm');
+
 var app=express();
 
 app.set('view engine','ejs')
@@ -11,61 +9,25 @@ app.listen(3000, function() {
     console.log('server running on port 3000'); 
 } ) 
   
-// Function callName() is executed whenever  
-// url is of the form localhost:3000/name 
+
 app.get("/",function(req,res){
     res.render("main.ejs");
 })
-app.get("/name",function(req,res){
-    facemask();
-    
+app.get("/name",function(req,res,err){
+    if(err){
+        res.redirect("/");
+}
+
+    facemask(req,res);
+    res.end();
+
 })
-app.post("/name",function(req,res){
-    end();
-    
-})
- 
-function facemask(req, res) { 
-    
-      
-    // Use child_process.spawn method from  
-    // child_process module and assign it 
-    // to variable spawn 
+
+function facemask(req, res,next) { 
     var spawn = require("child_process").spawn; 
-      
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
-      
-   
-    var process = spawn('python',["./detect_mask_video.py"] ); 
-  
-    // Takes stdout data from script which executed 
-    // with arguments and send this data to res object 
+    var process = spawn('python',["./detect_mask_video.py"] );
     process.stdout.on('data', function(data) { 
-        h.innercontent=data.toString('utf8');
-    } );
-} 
-function end(req, res) { 
+    res.send(data.toString('utf8'));
+ } );
     
-      
-    // Use child_process.spawn method from  
-    // child_process module and assign it 
-    // to variable spawn 
-    var spawn = require("child_process").spawn; 
-      
-    // Parameters passed in spawn - 
-    // 1. type_of_script 
-    // 2. list containing Path of the script 
-    //    and arguments for the script  
-      
-   
-    var process = spawn('python',["./run.py"] ); 
-  
-    // Takes stdout data from script which executed 
-    // with arguments and send this data to res object 
-    process.stdout.on('data', function(data) { 
-        h.innercontent=data.toString('utf8');
-    } );
 } 
